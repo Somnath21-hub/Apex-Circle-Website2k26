dde0e8c039bfacf0655d7df02bdfc4c5c2459150
Abhishek Kumar <abhishek9661342993@gmail.com>
2026-04-13 22:07:11 +0530

    fix: comment out background color in About component for improved styling

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -210,7 +210,7 @@ export default function About() {
         // markers: true,
       },
       opacity: 1,
-      backgroundColor: 'black',
+      // backgroundColor: 'black',
       y: 0,
       filter: 'blur(0px)',
       duration: 1,
